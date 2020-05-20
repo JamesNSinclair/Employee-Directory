ad23ed0acda4ceff835d4e1ca6b8099f0eb2e5c0
JamesNSinclair <53437429+JamesNSinclair@users.noreply.github.com>
2020-05-20 13:14:53 +0100

    Could this be it

--- js/script.js
+++ js/script.js
@@ -1,27 +1,3 @@
-/* let photoCaptions = [];
-
-$('.proj-pictures').each(function () {
-	let caption = $(this).children().attr('data-title').toLowerCase();
-	photoCaptions.push(caption);
-});
-
-$('#searching-section').keyup(function(){
-  const request =  $('#searching-section').val().toLowerCase();
-    for (let i = 0; i < photoCaptions.length; i ++) {
-      if (photoCaptions[i].indexOf(request) < 0) {
-      	$('.proj-pictures').eq(i).hide();
-      } else {
-      	$('.proj-pictures').eq(i).show();
-      }
-      }
-}); */
-
-// PROJECT 8 JAVASCRIPT
-
-/* let xhr = new XMLHttpRequest();
-let people = '<div class="pic-collection">'; */
-
-
 const profilepic = document.getElementById('profile-pic');
 const name = document.getElementById('name');
 const email = document.getElementById('email');
@@ -29,7 +5,7 @@ const town = document.getElementById('town');
 const contactInfo = 'https://randomuser.me/api/?results=12&inc=name,location,email,dob,cell,picture&?nat=us,gb';
 let modal = document.getElementById('modalDiv');
 
-
+//FETCH
 
 fetch(contactInfo)
 .then(response => response.json())
@@ -37,10 +13,6 @@ fetch(contactInfo)
 
 
 //FUNCTIONS
-
-
-//Make Divs
-
 
 function createData(data) {
 	for (let i = 0; i < data.length; i+=1) {
@@ -54,136 +26,126 @@ function createData(data) {
 
 //FUNCTION
 
-//  CREATING AN EMPTY DIV SO PREMADE ONES DON'T HAVE TO BE MADE
-
 
 function storeData(dataLengthCount, number, data) {
-const	goesHereNum =	'goesHere' + number;
-let goesHere = document.getElementById(goesHereNum);
-const html = `
-	<div class="employee" id=${number}>
-		<img class='profile-pic' src="${dataLengthCount.picture.large}">
-			<div class='employee-deets'>
-				<h4> ${dataLengthCount.name.title} ${dataLengthCount.name.first} ${dataLengthCount.name.last}</h4>
-				<p> ${dataLengthCount.email}</p>
-				<p> ${dataLengthCount.location.city}</p>
-				</div>
-	</div>
-	`;
 
-	goesHere.innerHTML = html;
+	const	goesHereNum =	'goesHere' + number;
+	let goesHere = document.getElementById(goesHereNum);
 
-	createLightbox(goesHereNum, dataLengthCount, data);
+	const html = `
+		<div class="employee" id=${number}>
+			<img class='profile-pic' src="${dataLengthCount.picture.large}">
+				<div class='employee-deets'>
+					<h4> ${dataLengthCount.name.title} ${dataLengthCount.name.first} ${dataLengthCount.name.last}</h4>
+					<p> ${dataLengthCount.email}</p>
+					<p> ${dataLengthCount.location.city}</p>
+					</div>
+		</div>`;
+
+		goesHere.innerHTML = html;
+
+		createLightbox(goesHereNum, dataLengthCount, data);
 
 
 }
 
 
 function createLightbox(goesHereNum, dataLengthCount, data) {
-let toChange = document.getElementById(goesHereNum);
+	let toChange = document.getElementById(goesHereNum);
 
-	toChange.addEventListener('click', e => {
- let hereItGoes = parseInt(e.target.id);
- modal.style.zIndex = 5;
- 	modal.style.visibility = "visible";
+		toChange.addEventListener('click', e => {
+		 let hereItGoes = parseInt(e.target.id);
+		 console.log(hereItGoes);
+		 modal.style.zIndex = 5;
+		 	modal.style.visibility = "visible";
 
- const html = `
- <div class="employeeModal">
- <span class="close">&times;</span>
- <span class="previous">&#60;</span>
- <span class="next">&#62;</span>
- <div class="modal-employee">
- 	<img class='profile-pic' src="${dataLengthCount.picture.large}">
- 			<h4> ${dataLengthCount.name.title} ${dataLengthCount.name.first} ${dataLengthCount.name.last}</h4>
- 			<p> ${dataLengthCount.email}</p>
- 			<p> ${dataLengthCount.location.city}</p>
- 			<p> ${dataLengthCount.phone}</p>
- 			<p> ${dataLengthCount.location.street} ${dataLengthCount.location.state} ${dataLengthCount.location.postcode}</p>
-	</div>
- </div>`;
+		 const html = `
+		 <div class="employeeModal">
+			 <span class="close">&times;</span>
+			 <span class="previous">&#60;</span>
+			 <span class="next">&#62;</span>
+			 <div class="modal-employee">
+			 	<img class='profile-pic' src="${dataLengthCount.picture.large}">
+					<div class="cardUnder">
+							<h4> ${dataLengthCount.name.title} ${dataLengthCount.name.first} ${dataLengthCount.name.last}</h4>
+				 			<p> ${dataLengthCount.email}</p>
+				 			<p> ${dataLengthCount.location.city}</p>
+				 			<p> ${dataLengthCount.cell}</p>
+				 			<p> ${dataLengthCount.location.street.number} ${dataLengthCount.location.street.name}</p>
+							<p> ${dataLengthCount.location.state} </p>
+							<p> ${dataLengthCount.location.postcode}</p>
+					</div>
+				</div>
+		 </div>`;
 
- 	modal.innerHTML = html;
+		 	modal.innerHTML = html;
 
-
-hereItGoes = 0;
-createScroll(data, hereItGoes, goesHereNum);
-})
+		hereItGoes += -1;
+		createScroll(data, hereItGoes, goesHereNum);
+	})
 
 }
 
 
 function createScroll(data, hereItGoes, goesHereNum) {
-let next = document.querySelector('.next');
-let previous = document.querySelector('.previous');
-let close = document.querySelector('.close');
+	let next = document.querySelector('.next');
+	let previous = document.querySelector('.previous');
+	let close = document.querySelector('.close');
 
-	close.addEventListener('click', e  => {
-		console.log('hello');
-		modal.style.visibility = "hidden";
-	 modal.style.zIndex = -1;
-		console.log('hello');
-	});
+			close.addEventListener('click', e  => {
+				console.log('hello');
+				modal.style.visibility = "hidden";
+			 modal.style.zIndex = -1;
+				console.log('hello');
+			});
 
+		next.addEventListener('click', e => {
+			if (hereItGoes < 11) {
+					hereItGoes += 1;
+				 createModal(hereItGoes, data, goesHereNum);
+				 createScroll(data, hereItGoes, goesHereNum);
+		 		} else {
+				 console.log('hello')
+				}
+		});
 
-next.addEventListener('click', e => {
-	hereItGoes += 1;
-	let dataLengthCount = data[hereItGoes]
-
-
-const html = `
-<div class="employeeModal">
-<span class="close">&times;</span>
-<span class="previous">&#60;</span>
-<span class="next">&#62;</span>
-<div class="modal-employee">
- <img class='profile-pic' src="${dataLengthCount.picture.large}">
-		 <h4> ${dataLengthCount.name.title} ${dataLengthCount.name.first} ${dataLengthCount.name.last}</h4>
-		 <p> ${dataLengthCount.email}</p>
-		 <p> ${dataLengthCount.location.city}</p>
-		 <p> ${dataLengthCount.phone}</p>
-		 <p> ${dataLengthCount.location.street} ${dataLengthCount.location.state} ${dataLengthCount.location.postcode}</p>
- </div>
-</div>`;
-
- modal.innerHTML = html;
-
-
-
- createScroll(data, hereItGoes, goesHereNum);
-
-});
-
-previous.addEventListener('click', e => {
-
-if (hereItGoes > 0) {
-
-	hereItGoes += -1;
-	let dataLengthCount = data[hereItGoes]
-
-const html = `
-<div class="employeeModal">
-<span class="close">&times;</span>
-<span class="previous">&#60;</span>
-<span class="next">&#62;</span>
-<div class="modal-employee">
- <img class='profile-pic' src="${dataLengthCount.picture.large}">
-		 <h4> ${dataLengthCount.name.title} ${dataLengthCount.name.first} ${dataLengthCount.name.last}</h4>
-		 <p> ${dataLengthCount.email}</p>
-		 <p> ${dataLengthCount.location.city}</p>
-		 <p> ${dataLengthCount.phone}</p>
-		 <p> ${dataLengthCount.location.street} ${dataLengthCount.location.state} ${dataLengthCount.location.postcode}</p>
- </div>
-</div>`;
-
- modal.innerHTML = html;
-
-
- createScroll(data, hereItGoes, goesHereNum);
-
-
-} else {
-	console.log('hello')
-}
-});
+		previous.addEventListener('click', e => {
+			if (hereItGoes > 0) {
+					hereItGoes += -1;
+		 			createModal(hereItGoes, data, goesHereNum);
+			  } else {
+				console.log('hello')
+			}
+		});
 
 };
+
+
+function createModal(hereItGoes, data, goesHereNum) {
+	console.log(hereItGoes);
+ let dataLengthCount = data[hereItGoes]
+
+const html = `
+<div class="employeeModal">
+	<span class="close">&times;</span>
+	<span class="previous">&#60;</span>
+	<span class="next">&#62;</span>
+		<div class="modal-employee">
+		<img class='profile-pic' src="${dataLengthCount.picture.large}">
+			<div class="cardUnder">
+				<h4> ${dataLengthCount.name.title} ${dataLengthCount.name.first} ${dataLengthCount.name.last}</h4>
+				<p> ${dataLengthCount.email}</p>
+				<p> ${dataLengthCount.location.city}</p>
+				<p> ${dataLengthCount.cell}</p>
+				<p> ${dataLengthCount.location.street.number}  ${dataLengthCount.location.street.name}</p>
+				<p> ${dataLengthCount.location.state}</p>
+				<p> ${dataLengthCount.location.postcode}</p>
+			</div>
+		</div>
+</div>`;
+
+modal.innerHTML = html;
+
+
+createScroll(data, hereItGoes, goesHereNum);
+}
